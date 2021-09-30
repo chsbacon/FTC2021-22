17786dcb04fbbaba573f0ae36489813ca6d4b670
japhero <wedertriplets@gmail.com>
2021-09-30 17:00:59 -0400

    hardware_map

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware_map.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class hardware_map {
+    public DcMotor motor1 = null;
+    public DcMotor motor2 = null;
+    public DcMotor motor3 = null;
+    public DcMotor motor4 = null;
+
+    private HardwareMap HWMap = null;
+    private ElapsedTime period = new ElapsedTime();
+
+    public hardware_map() {
+    }
+
+    public void init(HardwareMap ahwMap){
+        HWMap = ahwMap;
+        motor1 = HWMap.dcMotor.get("M1");
+        motor2 = HWMap.dcMotor.get("M2");
+        motor3 = HWMap.dcMotor.get("M3");
+        motor4 = HWMap.dcMotor.get("M4");
+    }
+}
