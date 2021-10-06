d1983c438a654bf191cad88803eb6ad49e13ddfb
grahamlenert <glenert41@charlottesvilleschools.org>
2021-10-06 16:55:35 -0400

    post wednesday 10/6 PID Code -- hasn't been tested -- kP,kI,kD variables are random (based off of Graham's PID Box). Seems to have tuning capabilities. Telemetries added.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RotatePIDtesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RotatePIDtesting.java
@@ -2,18 +2,17 @@ package org.firstinspires.ftc.teamcode;
 
 
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
+
 
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 import java.util.concurrent.TimeUnit;
@@ -24,48 +23,84 @@ public class RotatePIDtesting extends LinearOpMode {
 
     HardwareMap2022 robot = new HardwareMap2022();
 
-    double pwr = .5;
-    double target;
-    double kP = 0;
-    double kI = 0;
-    double kD = 0;
-
     public void runOpMode(){
 
         robot.init(hardwareMap);
 
+        waitForStart();
 
-        if(gamepad1.a){
-            rotateToHeadingV1(.5, 90);
+        while(opModeIsActive()){
+
+            double kP = .475;
+            double kI = .34;
+            double kD = .52;
+
+            if(gamepad1.x){
+                sleep(250);
+                kP = kP + .01;
+            }
+
+            if((gamepad1.x) && (gamepad1.left_bumper)){
+                sleep(250);
+                kP = kP - .01;
+            }
+
+
+            if(gamepad1.y){
+                sleep(250);
+                kI = kI + .01;
+            }
+            if((gamepad1.y) && (gamepad1.left_bumper)){
+                sleep(250);
+                kI = kI - .01;
+            }
+
+            if(gamepad1.a){
+                rotateToHeadingV1(.5, 90, kP, kI, kD);
+            }
+
+
+            if(gamepad1.b){
+                sleep(250);
+                kD = kD + .01;
+            }
+            if((gamepad1.b) && (gamepad1.left_bumper)){
+                sleep(250);
+                kD = kD - .01;
+            }
+
+            if(gamepad1.a){
+                rotateToHeadingV1(.25, 90, kP, kI, kD);
+            }
+
+            telemetry.addData("kP: ", "%.2f", kP);
+            telemetry.addData("kI: ", "%.2f", kI);
+            telemetry.addData("kD: ", "%.2f", kD);
+
+            telemetry.update();
+
         }
-
-        if(gamepad1.x){
-            kP = kP + .01;
-        }
-
-        if(gamepad1.y){
-            kI = kI + .01;
-        }
-
-        if(gamepad1.b){
-            kD = kD + .01;
-        }
-
-
 
 
     }
 
 
-    void rotateToHeadingV1(double pwr, double target){
+    void stopDriving(){
+        robot.frontLeftMotor.setPower(0);
+        robot.frontRightMotor.setPower(0);
+        robot.backLeftMotor.setPower(0);
+        robot.backRightMotor.setPower(0);
+    }
 
 
 
-        // set to a big number so it doesn't accidently match the target angle
-        //therefore hypothetically completing the while-loop accidently
+    void rotateToHeadingV1(double pwr, double target, double kP, double kI, double kD){
+
+        // set to a big number so it doesn't accidentally match the target angle
+        //therefore hypothetically completing the while-loop accidentally
         double currAng = 10000;
 
-        Orientation targetOrient;
+
         Orientation currOrient;
 
         double integralSum = 0;
@@ -78,7 +113,7 @@ public class RotatePIDtesting extends LinearOpMode {
 
 
 
-        while( currAng != target ){
+        while((currAng != target) && opModeIsActive()){
 
             pidTimer.reset();
 
@@ -86,22 +121,33 @@ public class RotatePIDtesting extends LinearOpMode {
             currAng = currOrient.angleUnit.DEGREES.normalize(currOrient.firstAngle);
 
             error = target - currAng;
-
             derivative = (error - lastError) / pidTimer.milliseconds();
-
             integralSum = integralSum + (error * pidTimer.time());
-
             out = (kP * error) + (kI * integralSum) + (kD * derivative);
-            //Still have to see if out is -1 < out < 1
+
+
+            telemetry.addData("target: ", "%.2f", target);
+            telemetry.addData("current: ", "%.2f", currAng);
+            telemetry.addData("out: ", "%.2f", out);
+            telemetry.update();
+
+            //robot.frontLeftMotor.setPower(pwr);
+            //robot.frontRightMotor.setPower(pwr);
+            //robot.backLeftMotor.setPower(pwr);
+            //robot.backRightMotor.setPower(pwr);
 
             robot.frontLeftMotor.setPower(pwr + out);
-            robot.frontRightMotor.setPower(pwr - out);
+            robot.frontRightMotor.setPower(pwr + out);
             robot.backLeftMotor.setPower(pwr + out);
-            robot.backRightMotor.setPower(pwr - out);
+            robot.backRightMotor.setPower(pwr + out);
 
             lastError = error;
 
         }
+        stopDriving();
+        telemetry.addData("target: ", "%.2f", target);
+        telemetry.addData("current: ", "%.2f", currAng);
+        telemetry.update();
     }
 
 
