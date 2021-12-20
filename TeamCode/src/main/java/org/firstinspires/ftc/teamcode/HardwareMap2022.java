d9d2ee70ff0ec4da4b4f5058260ebbdf11b180a4
grahamlenert <glenert41@charlottesvilleschools.org>
2021-12-20 14:33:07 -0500

    intakeMotor up/down added but untested

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMap2022.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMap2022.java
@@ -214,7 +214,7 @@ public class HardwareMap2022
         parameters.loggingTag          = "IMU";
         // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
         // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
-        // and named "imu".
+        // and named "imu"
         imu = hwMap.get(BNO055IMU.class, "imu");
         imu.initialize(parameters);
 
